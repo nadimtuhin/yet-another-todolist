bdfeed5102800e4f400c17612dfb27b09939d731
nadimtuhin <nadimtuhin@gmail.com>
2022-01-29 23:21:15 +0600

    fix: fixed redux-persist bug

--- redux/store.js
+++ redux/store.js
@@ -1,23 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { combineReducers } from 'redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import taskReducer from './tasksSlice';
 
 const persistConfig = {
-  key: 'root',
+  key: 'test',
   storage,
 };
 
-const reducer = {
+const reducer = combineReducers({
   tasks: taskReducer,
-};
+});
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+
 const persistor = persistStore(store);
 
 export { store, persistor };
