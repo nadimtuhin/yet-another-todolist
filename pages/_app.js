9f45f86d2f6f7598481f2d2673627bc5bff59d53
Omar Faruque Tuhin <omartuhin@pathao.com>
2022-01-29 20:19:46 +0600

    feat: added redux provider in app and added added taskSlice

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ redux/tasksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      const newTask = {
+        id: new Date(),
+        date: new Date(),
+        name: action.payload.task,
+      };
+      state.push(newTask);
+    },
+    deleteTask: (state, action) => state.filter((item) => item.id !== action.payload.id),
+  },
+});
+
+export const { addTask, deleteTask } = tasksSlice.actions;
+
+export default tasksSlice.reducer;
