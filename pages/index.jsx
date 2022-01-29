d63f5bda4b0557d691564c5166d2d1ffde353cba
nadimtuhin <nadimtuhin@gmail.com>
2022-01-29 23:21:33 +0600

    feat: added index page title

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,26 @@
+import Head from 'next/head';
+
 import AddTodo from '../components/home/AddTodo';
 import TodoList from '../components/home/TodoList';
 
 export default function Home() {
   return (
-    <div className="app-container">
-      <div className="app-wrapper">
-        <div className="mb-4">
-          <h1 className="app-title">Todo List</h1>
-          <div className="flex mt-4">
-            <AddTodo />
+    <>
+      <Head>
+        <title>Yet another Todo List app</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="app-container">
+        <div className="app-wrapper">
+          <div className="mb-4">
+            <h1 className="app-title">Todo List</h1>
+            <div className="flex mt-4">
+              <AddTodo />
+            </div>
           </div>
+          <TodoList />
         </div>
-        <TodoList />
       </div>
-    </div>
+    </>
   );
 }
