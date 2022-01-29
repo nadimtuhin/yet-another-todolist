be58e270268039ca67b50d33132942b1cadf60fa
Omar Faruque Tuhin <omartuhin@pathao.com>
2022-01-29 22:50:23 +0600

    feat: added redux localstorage

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import persistState from 'redux-localstorage';
+import { compose } from 'redux';
+
 import taskReducer from './tasksSlice';
+
+const enhancer = compose(
+  persistState(/* paths, config */),
+);
 
 export default configureStore({
   reducer: {
     tasks: taskReducer,
   },
+  enhancers: [enhancer],
 });
