26075d671d98fc7a31dc93110ae8f7f6061660bc
Omar Faruque Tuhin <omartuhin@pathao.com>
2022-01-29 23:23:07 +0600

    refactor

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import Head from 'next/head';
 
 import { store, persistor } from '../redux/store';
 import '../styles/globals.css';
@@ -10,6 +11,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <Component {...pageProps} />
       </PersistGate>
     </Provider>
