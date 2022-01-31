b9fac8c0589bc436e53df91d56ab434a58997ad8
nadimtuhin <nadimtuhin@gmail.com>
2022-01-31 21:17:18 +0600

    feat: updated eslint rules for react components

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,9 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     // allow jsx syntax in js files (for next.js project)
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // should add ".ts" if typescript project
+    "react/jsx-no-bind": [{
+      "allowArrowFunctions": true,
+      "allowFunctions": true,
+    }],
   },
 };
