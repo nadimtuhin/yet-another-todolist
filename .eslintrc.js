26c4b2bfdeb3ec68d1eb03fcc0621092a3d5ec61
Omar Faruque Tuhin <omartuhin@pathao.com>
2022-01-31 22:06:16 +0600

    fix: fix eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,9 +22,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     // allow jsx syntax in js files (for next.js project)
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // should add ".ts" if typescript project
-    "react/jsx-no-bind": [{
-      "allowArrowFunctions": true,
-      "allowFunctions": true,
-    }],
+    'react/jsx-no-bind': 'off',
   },
 };
